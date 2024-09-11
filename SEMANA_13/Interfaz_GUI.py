ae976118ee0c131d351bb83e0076e5c147a6b9d4
https://github.com/DavidUEA/Tareagym.git <dh.changoa@uea.edu.ec>
2024-09-10 19:06:05 -0500

    Merge remote-tracking branch 'origin/master'

--- SEMANA_13/Interfaz_GUI.py
+++ SEMANA_13/Interfaz_GUI.py
@@ -1,40 +1,56 @@
 import tkinter as tk
+from tkinter import messagebox
 
+#creamos una ventana
 app = tk.Tk()
-
-
+entrada = tk.StringVar(app)
 "dimension ancho x altura"
-app.geometry('800x800')
+app.geometry('700x700')
 app.configure(background='sky blue')
 tk.Wm.wm_title(app, "Programa de Interfaz")
-tk.Button(
+
+tk.Label(
     app,
+    text="Bienvenidos a nuestra primera aplicación de Interfaz",
+    bg="orange",
+    fg="White",
+    font=("Courier", 14),
+    justify="center"
+
+).pack(
+    fill=tk.BOTH,
+    expand=False,
+)
+
+def Llamar():
+    texto_ingrese = entrada.get()
+    messagebox.showinfo("llamar", message=f"{texto_ingrese}")
+
+
+
+tk.Button(app,
     text="Agregar informacion",
     font=("Courier", 14),
     bg="#00a8e8",
-    fg="White"
+    fg="White",
+    width=10,
+    height= 2,
+    command=Llamar
+
 ).pack(
     fill=tk.BOTH,
     expand=False
 )
 
-tk.Label(
+tk.Entry(
     app,
-    text="Ingrese",
-    bg="black",
+    bg="green",
     fg="White",
-    justify="center"
+    justify="center",
+    font=("Courier", 14),
+    textvariable=entrada
 ).pack(
     fill=tk.BOTH,
     expand=True,
-)
-tk.Entry(
-    bg="black",
-    fg="White",
-    justify="center",
-   ).pack(
-    fill=tk.BOTH,
-    expand=True,
-
 )
 app.mainloop()
